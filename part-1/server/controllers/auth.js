57a88fa494619f84ab7f7ef6caf5d00f8e248bcf
Hayden Sweet <haydensweet22@gmail.com>
2022-05-05 17:01:34 -0600

    lab done

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -3,20 +3,37 @@ const users = []
 
 module.exports = {
     login: (req, res) => {
-      console.log('Logging In User')
       console.log(req.body)
       const { username, password } = req.body
+
       for (let i = 0; i < users.length; i++) {
         if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
-        }
+          const authenticated = bcrypt.compareSync(password, users[i].passwordHash)
+          if (authenticated === true){
+          let userReturn = {...user[i]}
+          delete userReturn.passwordHash
+          res.status(200).send(userReturn)
+        } 
+       }
       }
       res.status(400).send("User not found.")
     },
     register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
+        const {username, email, firstName, lastName, password} = req.body
+        const salt = bcrypt.genSaltSync(5)
+        const passwordHash = bcrypt.hashSync(password, salt)
+
+      let user = {
+        username,
+        email,
+        firstName,
+        lastName,
+        passwordHash
+      }
+        users.push(user)
+        let userReturn = {...user}
+        delete userReturn.passwordHash
+        console.log(userReturn)
         res.status(200).send(req.body)
     }
 }
